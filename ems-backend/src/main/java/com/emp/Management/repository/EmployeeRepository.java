6451da8159297230ca5459ebbaa8e2aef1a9faa9
A-DILEEP <dileepalla2003@gmail.com>
2025-09-03 23:56:47 +0530

    styling

--- ems-backend/src/main/java/com/emp/Management/repository/EmployeeRepository.java
+++ ems-backend/src/main/java/com/emp/Management/repository/EmployeeRepository.java
@@ -1,11 +1,6 @@
-
-
 package com.emp.Management.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.emp.Management.entity.Employee;
-
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 	
