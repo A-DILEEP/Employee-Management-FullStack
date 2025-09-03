d974020776f11c6cf0bf84378dd110452e740da7
A-DILEEP <dileepalla2003@gmail.com>
2025-09-03 19:09:41 +0530

    deleted test file

--- ems-backend/src/test/java/com/emp/Management/controller/DepartmentController.java
+++
@@ -1,8 +0,0 @@
-package com.emp.Management.controller;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(DepartmentController.class)
-public class DepartmentController {
-	
-}
